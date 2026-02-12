b70112c4f05c703e1591084352837c4780a4cf7c
Sumit Kumar <sumitblock@gmail.com>
2026-02-12 09:15:58 -0600

    Implemented Image Transformations

--- Assignment_1/homework1_programming/image_transformations.py
+++ Assignment_1/homework1_programming/image_transformations.py
@@ -18,23 +18,50 @@ def inside_image(x, y, im):
 def transform(im, T):
     H = im.shape[0]
     W = im.shape[1]
-    im_new = np.zeros_like(im)
+    im_new = np.zeros_like(im, dtype=np.uint8)
+    # Precompute inverse of T for efficiency
+    T_inv = np.linalg.inv(T)
     # 1. Loop over each pixel in the output image
     for y_out in range(H):
         for x_out in range(W):
             # 2. Convert pixel to homogeneous coordinates
             p_out = np.array([x_out, y_out, 1], dtype=np.float32)
             # 3. Apply the inverse transformation to get the corresponding pixel in the input image
-            T_inv = np.linalg.inv(T)
-            p_in = np.matmul(T_inv, p_out)
-            x_in = p_in[0]
-            y_in = p_in[1]
+            p_in = T_inv @ p_out
+            # Normalise homogeneous coordinates
+            if abs(p_in[2]) < 1e-8:
+                continue
+            x_in = p_in[0] /p_in[2]
+            y_in = p_in[1] /p_in[2]
             # 4. Check if the pixel is within the bounds of the input image
             if inside_image(x_in, y_in, im):
                 # 5. If it is, copy the pixel value to the output image
-                im_new[y_out, x_out] = biliniar_sample(im, x_in, y_in)                
+                im_new[y_out, x_out] = bilinear_sample(im, x_in, y_in)                
     return im_new
 
+# bilinear sampling
+def bilinear_sample(im, x, y):
+    H, W = im.shape[0], im.shape[1]
+    x0 = int(np.floor(x))
+    y0 = int(np.floor(y))
+    x1 = x0 + 1
+    y1 = y0 + 1
+    # out of bounds error --> return black
+    if x0 < 0 or x1 >= W or y0 < 0 or y1 >= H:
+        return np.zeros((im.shape[2],), dtype=np.uint8)
+    
+    Ia = im[y0, x0].astype(np.float32)
+    Ib = im[y0, x1].astype(np.float32)
+    Ic = im[y1, x0].astype(np.float32)
+    Id = im[y1, x1].astype(np.float32)
+    wx = x - x0
+    wy = y - y0
+    wa = (1 - wx) * (1 - wy)
+    wb = wx * (1 - wy)
+    wc = (1 - wx) * wy
+    wd = wx * wy
+    val = (wa * Ia) + (wb * Ib) + (wc * Ic) + (wd * Id)
+    return val.astype(np.uint8)
 
 # main function
 # notice you cannot run this main function until you implement the above transform() function 
